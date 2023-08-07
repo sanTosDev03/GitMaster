ff26e02da6a936c71a4a9db5d4286599778efcbe
SantosDev <santosdevtech@gmail.com>
2023-08-07 16:20:10 -0300

    Nuevo commit ingreso al pais

--- gitmaster.js
+++ gitmaster.js
@@ -2,7 +2,11 @@ let nombre = "santiago";
 let apellido = "lescano";
 let edad = 20;
 let pais = "Argentina";
+let login = prompt("Por favor, introduce su nacionalidad:");
 
-console.log(nombre);
+if (login != "Argentina") {
+    console.log("No puedes ingresar al país");
+} else {
+    console.log("Bienvenido a la republica");
+}
 
-console.log("Hola " + nombre);
